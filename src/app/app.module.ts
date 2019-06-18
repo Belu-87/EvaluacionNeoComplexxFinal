64a34a2c58b8994e5affbc71ebf98c4a8f4d9a9d
Bel\Belu <mbblasquiz@gmail.com>
2019-06-17 22:46:20 -0300

    Install ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EmpleadosModule } from './empleados/empleados.module';
 import { AltaEmpleadoComponent } from './empleados/alta-empleado/alta-empleado.component';
 import { ListadoEmpleadosComponent } from './empleados/listado-empleados/listado-empleados.component';
@@ -13,7 +14,8 @@ import { ListadoEmpleadosComponent } from './empleados/listado-empleados/listado
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    EmpleadosModule
+    EmpleadosModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
